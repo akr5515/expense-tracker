f58af7236436750a0e0565fef816e8d762c97d87
akr5515 <akrangkon5515@gmail.com>
2023-11-12 22:49:30 +0600

    update

--- expense-tracker-FE/src/pages/expenses.page.tsx
+++ expense-tracker-FE/src/pages/expenses.page.tsx
@@ -1,7 +1,21 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const ExpensesPage = () => {
-  return <div>ExpensesPage</div>;
+  return (
+    <Box>
+      <Box>
+        <Typography variant="h4">Monthly Expense: </Typography>
+      </Box>
+      <Box>
+        <Typography variant="h4">Yearly Expense: </Typography>
+      </Box>
+
+      <Box>
+        <Typography variant="h5">Expense History </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default ExpensesPage;
